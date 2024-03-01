79ac3b605a92b73b58e7936552903da2115d1d9e
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-03-01 09:39:01 +0700

    style thread list

--- src/pages/ThreadList.jsx
+++ src/pages/ThreadList.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateFromApi } from "../slices/threads";
 import { Link } from "react-router-dom";
+import "./ThreadList.css";
 
 export function ThreadListPage() {
   const dispatch = useDispatch();
@@ -14,16 +15,22 @@ export function ThreadListPage() {
 
   return (
     <>
-      <ul>
+      <ul className="thread-list">
         {threads.map((thread) => (
-          <li key={thread.id}>
-            <h2>
-              <Link to={`/thread/${thread.id}`}>{thread.title}</Link>
+          <li className="thread-list-item" key={thread.id}>
+            <h2 className="thread-list-item--title">
+              <Link
+                className="thread-list-item--link"
+                to={`/thread/${thread.id}`}
+              >
+                {thread.title}
+              </Link>
             </h2>
             <p
               dangerouslySetInnerHTML={{
                 __html: thread.body,
               }}
+              className="thread-list-item--body"
             />
           </li>
         ))}
