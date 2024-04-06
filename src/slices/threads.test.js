e30944ba95ab12133779f905bd9141aed943ba35
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2024-04-06 14:01:34 +0700

    test: update threads thunk

--- src/slices/threads.test.js
+++ src/slices/threads.test.js
@@ -1,6 +1,10 @@
-import { describe, expect, it } from 'vitest';
+import {
+  beforeEach,
+  describe, expect, it, vi,
+} from 'vitest';
 import { createProcessState } from './utils';
 import threadsReducer, { updateThreads } from './threads';
+import { getData } from '../api';
 
 const blankState = {
   threads: {
@@ -9,7 +13,12 @@ const blankState = {
   },
 };
 
-describe('update threads test', () => {
+vi.mock('../api', async (importOriginal) => ({
+  ...await importOriginal(),
+  getData: vi.fn(),
+}));
+
+describe('update threads state', () => {
   it('loading state', () => {
     const state = threadsReducer(
       blankState,
@@ -41,5 +50,60 @@ describe('update threads test', () => {
     expect(state.threads.loading).toBe(false);
     expect(state.threads.error).toBeNull();
     expect(state.threads.list).toBe(list);
+  });
+});
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+describe('update threads thunk', () => {
+  it('dispatch fullfiled with right data', async () => {
+    const list = [
+      {
+        id: '1',
+      },
+      {
+        id: '2',
+      },
+    ];
+    let requestId;
+    const dispatch = vi.fn().mockImplementation(({ meta }) => {
+      requestId = meta.requestId;
+    });
+    getData.mockResolvedValueOnce({
+      threads: list,
+    });
+
+    await (updateThreads())(dispatch);
+
+    expect(dispatch).toHaveBeenNthCalledWith(
+      1,
+      updateThreads.pending(requestId),
+    );
+
+    expect(dispatch).toHaveBeenNthCalledWith(
+      2,
+      updateThreads.fulfilled(list, requestId),
+    );
+  });
+  it('dispatch rejected error', async () => {
+    const error = new Error('this is error');
+    let requestId;
+    const dispatch = vi.fn().mockImplementation(({ meta }) => {
+      requestId = meta.requestId;
+    });
+    getData.mockRejectedValueOnce(error);
+
+    await (updateThreads())(dispatch);
+
+    expect(dispatch).toHaveBeenNthCalledWith(
+      1,
+      updateThreads.pending(requestId),
+    );
+    expect(dispatch).toHaveBeenNthCalledWith(
+      2,
+      updateThreads.rejected(error, requestId),
+    );
   });
 });
